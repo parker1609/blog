6734e4af995d265f2e081565f71d7bdf4458286a
parker1609 <psbum1609@gmail.com>
2022-10-09 22:00:34 +0900

    rebuilding parker1609 blog, 2022년 10월  9일 일요일 22시 00분 34초 KST

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+echo "=== Step1. build hugo"
 # Build the project.
 # hugo -t <your theme>
 hugo -t PaperMod
 
+echo "=== Step2. commit & push public directory"
 # Go To Public folder, sub module commit
 cd public
 # Add changes to git.
@@ -21,9 +23,9 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin main
 
+echo "=== Step3. commit & push blog directory"
 # Come Back up to the Project Root
 cd ..
-
 
 # blog repository Commit & Push
 git add .
